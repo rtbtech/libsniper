95067ad2046f3f498ab876409e0aa7d178b64477
Oleg Romanenko <oleg@romanenko.ro>
2023-01-21 15:32:54 +0300

    update

--- sniper/pico/common.h
+++ sniper/pico/common.h
@@ -21,8 +21,8 @@
 
 namespace sniper::pico {
 
-static const size_t MAX_HEADERS = 32u;
-static const size_t MAX_PARAMS = 32u;
+static const size_t MAX_HEADERS = 64u;
+static const size_t MAX_PARAMS = 64u;
 
 enum class ParseResult
 {
