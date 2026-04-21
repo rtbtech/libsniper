41d8c53b29eacc8976575339e5d445f4c6d22fa7
Oleg Romanenko <oleg@romanenko.ro>
2026-04-21 14:38:17 +0300

    update proto

--- sniper/std/map.h
+++ sniper/std/map.h
@@ -26,4 +26,26 @@ using std::multimap;
 using std::unordered_map;
 using std::unordered_multimap;
 
+struct string_hash
+{
+    using hash_type = std::hash<std::string_view>;
+    using is_transparent = void;
+
+    std::size_t operator()(const char* str) const
+    {
+        return hash_type{}(str);
+    }
+    std::size_t operator()(std::string_view str) const
+    {
+        return hash_type{}(str);
+    }
+    std::size_t operator()(std::string const& str) const
+    {
+        return hash_type{}(str);
+    }
+};
+
+template<typename T>
+using string_map = std::unordered_map<std::string, T, string_hash, std::equal_to<>>;
+
 } // namespace sniper
